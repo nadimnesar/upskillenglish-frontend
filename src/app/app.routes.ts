dea71e8dceff4c28bea28e5529ad7c9b191bed81
nadimnesar <nadimnesar.cse@gmail.com>
2024-07-17 23:54:40 +0600

    navbar added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LeaderboardComponent } from './pages/leaderboard/leaderboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'leaderboard', component: LeaderboardComponent },
+];
