702271942fcbbfc9ebf9b7009d29e01d1b6e3c08
nadimnesar <nadimnesar.cse@gmail.com>
2024-07-28 19:57:30 +0600

    login related functions implemented

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +8,29 @@ import { Observable } from 'rxjs';
 export class AuthService {
   private apiUrl = 'http://localhost:8080/api/auth';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   register(username: string, password: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/register`, { username, password });
   }
 
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { username, password }).pipe(
+      tap((response: any) => {
+        localStorage.setItem('jwt', response.jwt);
+      })
+    );
+  }
+
+  getJwtToken(): string | null {
+    return localStorage.getItem('jwt');
+  }
+
+  logout(): void {
+    localStorage.removeItem('jwt');
+  }
+
+  isLoggedIn(): boolean {
+    return localStorage.getItem('jwt') !== null;
+  }
 }
