b1e6bcf6ea8a880689d811548aef088a99402a67
nadimnesar <nadimnesar.cse@gmail.com>
2024-07-28 19:58:46 +0600

    fully functional navbar upgrade (logout not work)

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(private router: Router) { }
 
+  isLoginRoute(): boolean {
+    return this.router.url === '/login';
+  }
+
+  isHomeRoute(): boolean {
+    return this.router.url === '/';
+  }
+
+  isSignupRoute(): boolean {
+    return this.router.url === '/signup';
+  }
+
+  isLeaderboardRoute(): boolean {
+    return this.router.url === '/leaderboard';
+  }
 }
