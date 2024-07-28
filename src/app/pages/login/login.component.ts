b346643b19403c30a2c4317e8c7e4f01eff40103
nadimnesar <nadimnesar.cse@gmail.com>
2024-07-28 18:01:55 +0600

    registration service, error massage added.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  successMessage: string = '';
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      if (params['registered'] === 'true') {
+        this.successMessage = 'Registration complete. Please login.';
+      }
+    });
+  }
+  
 }
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -9,16 +10,20 @@ import { AuthService } from '../../services/auth.service';
 export class SignupComponent {
   username: string = '';
   password: string = '';
+  errorMessage: string = '';
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   register(): void {
     this.authService.register(this.username, this.password).subscribe({
       next: (response) => {
         console.log('Registration successful', response);
+        this.errorMessage = '';
+        this.router.navigate(['/login'], { queryParams: { registered: 'true' } });
       },
       error: (error) => {
         console.error('Registration failed', error);
+        this.errorMessage = error.error.message || 'Registration failed. Please try again.';
       },
       complete: () => {
         console.info('Registration process completed');
