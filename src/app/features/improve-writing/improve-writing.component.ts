817352b55261414e937cfcfde4da76394992b26f
nadimnesar <nadimnesar.cse@gmail.com>
2024-07-28 19:58:19 +0600

    improve-writing.component and quick-practice.component files

---
+++ src/app/features/improve-writing/improve-writing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-improve-writing',
+  templateUrl: './improve-writing.component.html',
+  styleUrl: './improve-writing.component.scss'
+})
+export class ImproveWritingComponent {
+
+}
---
+++ src/app/features/quick-practice/quick-practice.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-practice',
+  templateUrl: './quick-practice.component.html',
+  styleUrl: './quick-practice.component.scss'
+})
+export class QuickPracticeComponent {
+
+}
