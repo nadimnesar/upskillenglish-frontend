c010e9db4d0333eb4702748accf2176af81609e3
nadimnesar <nadimnesar.cse@gmail.com>
2024-07-28 22:39:52 +0600

    added comments for understand the concept

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -2,16 +2,26 @@ import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from
 import { AuthService } from "./auth.service";
 import { Injectable } from "@angular/core";
 
+/**
+ * Injectable: This decorator marks the class as one that participates in the dependency injection system.
+ * providedIn: 'root': This configuration ensures that the AuthGuard service is available application - wide and
+ * follows the singleton pattern, meaning there will be only one instance of this service throughout the application.
+ */
 @Injectable({
     providedIn: 'root'
-  })
+})
 export class AuthGuard implements CanActivate {
 
     constructor(private authService: AuthService, private router: Router) { }
 
-    canActivate(
-        route: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot): boolean {
+    /**
+     * @param route An instance of ActivatedRouteSnapshot,
+     * This contains the future route that will be activated if the guard allows it.
+     * @param state An instance of RouterStateSnapshot,
+     * This contains the future state of the router if the guard allows the route to be activated.
+     * @returns true if the route can be activated, false if the route cannot be activated.
+     */
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
         if (this.authService.isLoggedIn()) {
             return true;
         } else {
