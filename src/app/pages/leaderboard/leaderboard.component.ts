1ad5017e28170a09e467f3e0f1d4634cb081b191
nadimnesar <nadimnesar.cse@gmail.com>
2024-08-25 02:41:18 +0600

    leaderboard implemented

--- src/app/pages/leaderboard/leaderboard.component.ts
+++ src/app/pages/leaderboard/leaderboard.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
-  styleUrl: './leaderboard.component.scss'
+  styleUrls: ['./leaderboard.component.scss']
 })
-export class LeaderboardComponent {
+export class LeaderboardComponent implements OnInit {
+  userList: any[] = [];
+  loading: boolean = true;
+  currentPage: number = 0;
+  hasNextPage: boolean = true;
+  pageSize: number = 10;
+  totalUsers: number = 0;
 
+  private apiUrl = 'http://localhost:8080/api/public/v1/get-all-user';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.loadPage(this.currentPage);
+  }
+
+  loadPage(page: number): void {
+    if (page < 0) return;
+    this.loading = true;
+    this.http.get<any[]>(`${this.apiUrl}?page=${page}&size=${this.pageSize}`)
+      .pipe(
+        catchError(error => {
+          console.error('Error fetching data', error);
+          this.loading = false;
+          return of([]);
+        })
+      )
+      .subscribe(response => {
+        this.userList = response;
+        this.hasNextPage = response.length === this.pageSize;
+        this.currentPage = page;
+        this.loading = false;
+      });
+  }
+
+  getGlobalRank(index: number): number {
+    return (this.currentPage * this.pageSize) + index + 1;
+  }
 }
