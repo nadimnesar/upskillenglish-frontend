7036ab6fa807f4c3958d9870d03811d267a8eb82
FI-Nabil <finabil98@gmail.com>
2024-09-22 16:26:32 +0600

    reading-test-phase-1

--- src/app/features/reading-test/reading-test.component.ts
+++ src/app/features/reading-test/reading-test.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-reading-test',
   templateUrl: './reading-test.component.html',
-  styleUrl: './reading-test.component.scss'
+  styleUrls: ['./reading-test.component.scss']
 })
-export class ReadingTestComponent {
+export class ReadingTestComponent implements OnInit {
 
+  passage: string = '';
+  mcqResponse: any = null;
+  opinionativeResponse: any = null;
+  factCheckResponse: any = null;
+  mcqSelected: string[] = [];
+  opinionativeSelected: string[] = [];
+  factCheckSelected: string[] = [];
+
+  private apiUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadTest();
+  }
+
+  loadTest(): void {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
+      'Content-Type': 'application/json'
+    });
+
+    // Sample API Call for fetching test data
+    this.http.get<any>(`${this.apiUrl}/api/public/v2/generate-passage`, { headers }).subscribe(response => {
+      this.passage = response.passage.passage;
+      //console.log(this.passage);
+      this.mcqResponse = response.mcq;
+      this.opinionativeResponse = response.opinionative;
+      this.factCheckResponse = response.factCheck;
+    });
+  }
 }
